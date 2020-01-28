17f60f3f33c374d94fb1dd47439ab50a8d4f1cb6
Daniel Pagan <dpagan@gatech.edu>
2020-01-28 10:38:23 -0600

    Changing plugins to not use docker and assume dependencies are met (basic checking)

--- plugins/nikto.fish
+++ plugins/nikto.fish
@@ -1,16 +1,20 @@
 #!/usr/local/bin/fish
 #
-# A script to launch nikto and compile the results
+# A plugin for plonkhonk to launch nikto and compile the results
 # Author: Daniel Pagan
 
 echo ===========================
 echo "Running nikto scan plugin"
 echo ===========================
 
-docker pull frapsoft/nikto
+# check for nikto
+if which nikto
+	echo "Error: nikto is either not installed or in the path"
+end
+
 for port in $port_list
-	echo "docker run frapsoft/nikto -host $target -port $port 2>&1 | tee $outdir/nikto-$port.txt"
-	docker run frapsoft/nikto -host $target -port $port 2>&1 | tee $outdir/nikto-$port.txt
+	echo "nikto -host $target -port $port -output $outdir/nikto-$port.txt"
+	nikto -host $target -port $port -output $outdir/nikto-$port.txt
 end
 
 echo ===== nikto scan plugin ======
